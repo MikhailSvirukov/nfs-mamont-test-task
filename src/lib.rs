d49b01674c84d8d20c72abeb3464c7e861671f78
Mikhail <mihailsvirukov@mail.ru>
2025-06-29 23:28:32 +0300

    fix mutex usage apply multiple action test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+mod unit;
+
 use std::cmp::min;
 use std::collections::HashMap;
+use std::sync::Mutex;
 
 #[derive(Debug)]
 pub enum Error {
@@ -8,6 +11,7 @@ pub enum Error {
     IncorrectOffset
 }
 
+#[allow(async_fn_in_trait)]
 pub trait FileSystemInterface {
     async fn touch(&mut self, path: &str)-> Result<(), Error>;
     async fn write(&mut self, path: &str, offset: usize, data: &[u8]) -> Result<(), Error>;
@@ -19,14 +23,14 @@ pub struct File {
 }
 
 pub struct FileSystem {
-    pub(crate) hashtable: HashMap<String, File>
+    pub hashtable: HashMap<String, Mutex<File>>
 }
 
 
 impl FileSystemInterface for FileSystem {
     async fn touch(&mut self, path: &str)-> Result<(), Error> {
         let new_file = File {content: Vec::new(), size: 0};
-        match self.hashtable.insert(path.to_string(), new_file) {
+        match self.hashtable.insert(path.to_string(), Mutex::new(new_file)) {
             Some(_) => panic!("{1}: {:?}", Error::FileExist
                               ,"file already exist"),
             None => Ok(())
@@ -41,14 +45,15 @@ impl FileSystemInterface for FileSystem {
                     ,"file not exist")
             }
             Some(_) => {
-                if offset > result.unwrap().size
+                let res=result.unwrap().get_mut().unwrap();
+                if offset > res.size
                 {
                     panic!("{1}: {:?}", Error::IncorrectOffset
                            ,"Offset more than file size")
                 }
-                let r=[self.hashtable[path].content[0..offset].to_owned(), Vec::from(data)].concat();
+                let r=[res.content[0..offset].to_owned(), Vec::from(data)].concat();
                 let new_file = File {content: r, size: offset+data.len()};
-                match self.hashtable.insert(path.to_string(), new_file) {
+                match self.hashtable.insert(path.to_string(), Mutex::from(new_file)) {
                     Some(_) => Ok(()),
                     None => Err(Error::FileNotExist)
                 }
@@ -58,21 +63,21 @@ impl FileSystemInterface for FileSystem {
     }
 
     async fn read(&mut self, path: &str, offset: usize, len: usize) -> Result<Vec<u8>, Error> {
-        let result= self.hashtable.get(path);
+        let result= self.hashtable.get_mut(path);
         match result {
             None => {
                 panic!("{1}: {:?}", Error::FileNotExist
                        ,"file not exist")
             }
             Some(_) => {
-                if offset > result.unwrap().size
+                let res=result.unwrap().get_mut().unwrap();
+                if offset > res.size
                 {
                     panic!("{1}: {:?}", Error::IncorrectOffset
                            ,"Offset more than file size")
                 }
-                let content = &result.unwrap()
-                    .content[offset..min(offset+len, result.unwrap().size)];
-                Ok(content.to_owned())
+                let result = &res.content[offset..min(res.size, offset+len)];
+                Ok(result.to_owned())
             }
         }
     }
--- src/unit.rs
+++ src/unit.rs
@@ -1,65 +1,81 @@
 use std::collections::HashMap;
-use std::sync::Mutex;
-use crate::funcs::{FileSystem, FileSystemInterface};
+use crate::{FileSystem, FileSystemInterface};
 
 #[tokio::test]
 async fn simple_read_after_write() {
-    let file_system=
-        Mutex::new(FileSystem {hashtable: HashMap::new()});
-    file_system.lock().unwrap().touch("/text.txt").await.unwrap();
-    file_system.lock().unwrap().write("/text.txt", 0, b"text").await.unwrap();
+    let mut file_system =
+        FileSystem {hashtable: HashMap::new()};
+    file_system.touch("/text.txt").await.unwrap();
+    file_system.write("/text.txt", 0, b"text").await.unwrap();
     let content=
-        file_system.lock().unwrap().read("/text.txt", 0, 4).await.unwrap();
+        file_system.read("/text.txt", 0, 4).await.unwrap();
     assert_eq!(content, b"text");
 }
 
 
 #[tokio::test]
 async fn three_writes() {
-    let file_system=
-        Mutex::new(FileSystem {hashtable: HashMap::new()});
-    file_system.lock().unwrap().touch("/text.txt").await.unwrap();
-    file_system.lock().unwrap().write("/text.txt", 0, b"text").await.unwrap();
-    file_system.lock().unwrap().write("/text.txt", 0, b"more and more").await.unwrap();
-    file_system.lock().unwrap().write("/text.txt", 5, b"haha").await.unwrap();
+    let mut file_system =
+       FileSystem {hashtable: HashMap::new()};
+    file_system.touch("/text.txt").await.unwrap();
+    file_system.write("/text.txt", 0, b"text").await.unwrap();
+    file_system.write("/text.txt", 0, b"more and more").await.unwrap();
+    file_system.write("/text.txt", 5, b"haha").await.unwrap();
     let content=
-        file_system.lock().unwrap().read("/text.txt", 0, 9).await.unwrap();
+        file_system.read("/text.txt", 0, 9).await.unwrap();
     assert_eq!(content, b"more haha");
 }
 
 #[tokio::test]
 #[should_panic(expected = "file not exist")]
 async fn error_no_file() {
-    let file_system=
-        Mutex::new(FileSystem {hashtable: HashMap::new()});
-    file_system.lock().unwrap().touch("/text.txt").await.unwrap();
-    file_system.lock().unwrap().read("/some.txt", 5,7).await.unwrap();
+    let mut file_system =
+       FileSystem {hashtable: HashMap::new()};
+    file_system.touch("/text.txt").await.unwrap();
+    file_system.read("/some.txt", 5,7).await.unwrap();
 }
 
 #[tokio::test]
 #[should_panic(expected = "Offset more than file size")]
 async fn error_incorrect_offset_read() {
-    let file_system=
-        Mutex::new(FileSystem {hashtable: HashMap::new()});
-    file_system.lock().unwrap().touch("/some.txt").await.unwrap();
-    file_system.lock().unwrap().write("/some.txt", 0,b"testing").await.unwrap();
-    file_system.lock().unwrap().read("/some.txt", 8,5).await.unwrap();
+    let mut file_system=
+        FileSystem {hashtable: HashMap::new()};
+    file_system.touch("/some.txt").await.unwrap();
+    file_system.write("/some.txt", 0,b"testing").await.unwrap();
+    file_system.read("/some.txt", 8,5).await.unwrap();
 }
 
 #[tokio::test]
 #[should_panic(expected = "Offset more than file size")]
 async fn error_incorrect_offset_write() {
-    let file_system=
-        Mutex::new(FileSystem {hashtable: HashMap::new()});
-    file_system.lock().unwrap().touch("/some.txt").await.unwrap();
-    file_system.lock().unwrap().write("/some.txt", 7,b"testing").await.unwrap();
+    let mut file_system=
+        FileSystem {hashtable: HashMap::new()};
+    file_system.touch("/some.txt").await.unwrap();
+    file_system.write("/some.txt", 7,b"testing").await.unwrap();
 }
 
 #[tokio::test]
 #[should_panic(expected = "file already exist")]
 async fn error_existed_file() {
-    let file_system=
-        Mutex::new(FileSystem {hashtable: HashMap::new()});
-    file_system.lock().unwrap().touch("/some.txt").await.unwrap();
-    file_system.lock().unwrap().touch("/some.txt").await.unwrap();
+    let mut file_system = FileSystem {hashtable: HashMap::new()};
+    file_system.touch("/some.txt").await.unwrap();
+    file_system.touch("/some.txt").await.unwrap();
+}
+
+#[tokio::test]
+async fn concurrent_actions() {
+    let mut file_system = FileSystem {hashtable: HashMap::new()};
+    file_system.touch("/some1.txt").await.unwrap();
+    file_system.touch("/some2.txt").await.unwrap();
+    file_system.touch("/some3.txt").await.unwrap();
+    file_system.write("/some1.txt", 0, b"some1").await.unwrap();
+    file_system.write("/some2.txt", 0, b"some2").await.unwrap();
+    file_system.write("/some3.txt", 0, b"some3").await.unwrap();
+    file_system.write("/some2.txt", 2, b"nne").await.unwrap();
+    let res2 = file_system.read("/some2.txt", 0, 5).await.unwrap();
+    let res1 = file_system.read("/some1.txt", 0, 5).await.unwrap();
+    let res3 = file_system.read("/some3.txt", 0, 5).await.unwrap();
+    assert_eq!(res2, b"sonne");
+    assert_eq!(res1, b"some1");
+    assert_eq!(res3, b"some3");
 }
